a8916d18322565c0fdb803d9ef1368991ccaedbd
Lucas <lucaskfm@gmail.com>
2024-08-24 16:26:49 -0300

    Created Layout for Product Detail

---
+++ ChefDelivery/ProductsView/ProductDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailView.swift
+//  ChefDelivery
+//
+//  Created by Lucas on 24/08/24.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailView()
+}

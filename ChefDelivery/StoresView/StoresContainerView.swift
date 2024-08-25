3a1709079d5039aaaaebcf448e7ea1dd3bf5e72c
Lucas <lucaskfm@gmail.com>
2024-08-25 10:14:55 -0300

    Added Clear Filter

--- ChefDelivery/StoresView/StoresContainerView.swift
+++ ChefDelivery/StoresView/StoresContainerView.swift
@@ -8,15 +8,56 @@
 import SwiftUI
 
 struct StoresContainerView: View {
+    
+    @State private var ratingFilter = 0
+    var filteredStores: [StoreType] {
+        return storesMock.filter { store in
+            store.stars >= ratingFilter
+        }
+    }
+    
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Lojas")
-            VStack (alignment: .leading, spacing: 20){
-                ForEach(storesMock) { store in
-                    NavigationLink {
-                        StoreDetailView(store: store)
+            HStack {
+                Text("Lojas")
+                Spacer()
+                Menu {
+                    Button {
+                        ratingFilter = 0
                     } label: {
-                        StoresItemView(storeItem: store)
+                        Text("Limpar Filtro")
+                    }
+                    Divider()
+                    ForEach(1...5, id: \.self) { rating in
+                        Button {
+                            ratingFilter = rating
+                        } label: {
+                            if (rating > 1) {
+                                Text("\(rating) estrelas ou mais")
+                            } else {
+                                Text("\(rating) estrela ou mais")
+                            }
+                        }
+                    }
+                } label: {
+                    Label("Menu", systemImage: "chart.bar.horizontal.page")
+                }
+            }
+            VStack (alignment: .leading, spacing: 20){
+                if filteredStores.isEmpty {
+                    Text ("Nenhum resultado encontrado.")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorRed"))
+                        .padding(.vertical, 32)
+                        .frame(maxWidth: .infinity)
+                } else {
+                    ForEach(filteredStores) { store in
+                        NavigationLink {
+                            StoreDetailView(store: store)
+                        } label: {
+                            StoresItemView(storeItem: store)
+                        }
                     }
                 }
             }

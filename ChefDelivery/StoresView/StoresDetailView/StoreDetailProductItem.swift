828451d37f7833fb0980e6980475da7eb7cd231a
Lucas <lucaskfm@gmail.com>
2024-08-25 10:50:32 -0300

    Refactoring Store Components

---
+++ ChefDelivery/StoresView/StoresDetailView/StoreDetailProductItem.swift
@@ -0,0 +1,18 @@
+//
+//  StoreDetailProductItem.swift
+//  ChefDelivery
+//
+//  Created by Lucas on 25/08/24.
+//
+
+import SwiftUI
+
+struct StoreDetailProductItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreDetailProductItem()
+}
---
+++ ChefDelivery/StoresView/StoresDetailView/StoresDetailHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  StoresDetailHeaderView.swift
+//  ChefDelivery
+//
+//  Created by Lucas on 25/08/24.
+//
+
+import SwiftUI
+
+struct StoresDetailHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoresDetailHeaderView()
+}

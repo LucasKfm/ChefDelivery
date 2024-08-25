c5f881dd57629a83e2fc23c48ff9815ffaad0810
Lucas <lucaskfm@gmail.com>
2024-08-25 10:56:28 -0300

    Refactoring Product Detail Components

---
+++ ChefDelivery/ProductsView/ProductDetailQuantityView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailQuantityView.swift
+//  ChefDelivery
+//
+//  Created by Lucas on 25/08/24.
+//
+
+import SwiftUI
+
+struct ProductDetailQuantityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailQuantityView()
+}
